7cd77b494fb76fa01a9a05de50514b087066c585
sujan bose <112420725+sujanboseb@users.noreply.github.com>
2024-08-27 12:36:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ async function sendMessageToUser(phoneNumber, message) {
 
 // Function to process messages and send requests to the external API
 async function processMessageWithApi(message) {
-  const apiUrl = "https://35b5-34-138-39-113.ngrok-free.app/predict";
+  const apiUrl = "https://62b3-34-138-39-113.ngrok-free.app/predict";
   try {
     // Call the prediction service
     const response = await axios.post(apiUrl, { text: message }, {
